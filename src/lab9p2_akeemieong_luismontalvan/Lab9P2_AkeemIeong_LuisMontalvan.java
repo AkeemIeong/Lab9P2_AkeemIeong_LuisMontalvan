0fced367abadb3269136a5aece1596f0c04c91af
Akeem Ieong Ham <akeemieong@gmail.com>
2023-06-16 15:02:31 -0600

    primer commit

---
+++ src/lab9p2_akeemieong_luismontalvan/Lab9P2_AkeemIeong_LuisMontalvan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab9p2_akeemieong_luismontalvan;
+
+/**
+ *
+ * @author SURFACEB2I7
+ */
+public class Lab9P2_AkeemIeong_LuisMontalvan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
